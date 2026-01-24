61bbcc23ed1219c3ecea7f3cb982b2e00a38327d
mmadden2011 <mmadden2011@outlook.com>
2026-01-24 17:58:39 -0500

    remove players map from FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,14 +34,6 @@ export default function App() {
 
       {status === "up" && (
         <>
-          <ul>
-            {players.map((p) => (
-              <li key={p.id}>
-                {p.first_name} {p.last_name}
-              </li>
-            ))}
-          </ul>
-
           <Simulation players={players} />
         </>
       )}
