72256ff1052e68858c0c74d14c51bee022c18b57
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-02-15 14:00:02 -0500

    Add home page buttons and wip date picker

--- pitchcraft-webserver/frontend/src/pages/Home.tsx
+++ pitchcraft-webserver/frontend/src/pages/Home.tsx
@@ -1,5 +1,25 @@
-import Button from '@mui/material/Button';
+import { useNavigate } from "react-router";
+import { Button } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
-export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+export default function Home() {
+  let navigate = useNavigate();
+
+  return <div>
+    <Button variant="contained" onClick={() => navigate("/guide")}>User Guide</Button>
+    <Button variant="contained" onClick={() => navigate("/simulation")}>Simulation</Button>
+    <br />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        defaultValue={dayjs()}
+        onChange={(newValue) => {
+          setValue(newValue);
+          console.log("Date changed: " + newValue);
+        }
+      }/>
+    </LocalizationProvider>
+  </div>
 }
