100b43d25e035e52c6484c9bb85671ab9025b7c2
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-04-21 02:13:45 -0400

    Add out type selector

--- pitchcraft-webserver/frontend/src/pages/Pregame.tsx
+++ pitchcraft-webserver/frontend/src/pages/Pregame.tsx
@@ -2,19 +2,26 @@ import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router";
 import {
     Divider,
+  FormControl,
+  FormLabel,
   Grid,
+  MenuItem,
   Paper,
+  Select,
   Typography,
 } from "@mui/material";
 import PlayerComboBox from "../components/PlayerComboBox";
 import { Player } from "../types";
 import PreGameBatter from "../components/PreGameBatter";
 
+type OutType = "default" | "ground" | "fly" | "strike";
+
 export default function Pregame() {
   const navigate = useNavigate();
   const state = useLocation().state;
   const [players, setPlayers] = useState<Player[]>([]);
   const [selectedPlayers, setSelectedPlayers] = useState<Set<number>>(new Set());
+  const [outTypes, setOutTypes] = useState<OutType[]>(Array(9).fill("default"));
 
   function updatePlayer(index: number, player: Player) {
     const oldPlayer = players[index];
@@ -28,6 +35,12 @@ export default function Pregame() {
     tempSelected.delete(oldPlayer.id);
     tempSelected.add(player.id);
     setSelectedPlayers(tempSelected);
+  }
+
+  function updateOutTypes(index: number, outType: OutType) {
+    const temp = [...outTypes];
+    temp[index] = outType;
+    setOutTypes(temp);
   }
 
   useEffect(() => {
@@ -50,16 +63,27 @@ export default function Pregame() {
   const batters = [];
   for (let i = 1; i < players.length; i++) {
     reports.push(<PreGameBatter pitcher={players[0]} batter={players[i]} key={"report" + i} />);
-    batters.push(<PlayerComboBox
-                   value={players[i]}
-                   teamId={battingTeam}
-                   batters={true}
-                   alreadySelected={selectedPlayers}
-                   onChange={(newValue) => { updatePlayer(i, newValue!) }}
-                   key={"batter" + i}
-                 />);
+    batters.push(
+      <div key={"batterDiv" + i}>
+        <PlayerComboBox
+          value={players[i]}
+          teamId={battingTeam}
+          batters={true}
+          alreadySelected={selectedPlayers}
+          onChange={(newValue) => { updatePlayer(i, newValue!) }}
+          key={"batter" + i}
+        />
+          <FormControl fullWidth size="small" sx={{ flex: 1 }}>
+            <Select value={outTypes[i-1]} onChange={(e) => updateOutTypes(i-1, e.target.value)}>
+              <MenuItem value="default">Default</MenuItem>
+              <MenuItem value="ground">Groundout</MenuItem>
+              <MenuItem value="fly">Flyout</MenuItem>
+              <MenuItem value="strike">Strikeout</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+    );
   }
-
 
   return <Paper sx={{ p: 2 }}>
     <Grid container spacing={2}>
